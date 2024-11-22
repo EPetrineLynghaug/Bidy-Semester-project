37101a292ecf17001f64caca8fde4ce07abb08a0
EPetrineLynghaug <136695793+EPetrineLynghaug@users.noreply.github.com>
2024-11-22 10:21:45 +0100

    Refactor action buttons section with temporary TailwindCSS styling; ready for further improvements next week.

--- src/js/components/myAuctions.component.js
+++ src/js/components/myAuctions.component.js
@@ -10,31 +10,34 @@ export function myAuctions(listing) {
 
   const listIthem = document.createElement("div");
   listIthem.className =
-    "auction-card border rounded-lg shadow-lg p-4 bg-white flex flex-col gap-4";
+    "auction-card   p-4  flex flex-row gap-4 border-b-2 border-blue-800 ";
 
   listIthem.innerHTML = `
-   <div class =" w-full h-48 overflow-hidden roundet.lg">
-     <img src="${
-       media[0] ? media[0].url : "https://via.placeholder.com/400x300"
-     }"
-          alt="${media[0] ? media[0].alt : "Auction image"}"
-          class="w-full h-full object-cover rounded-lg shadow-lg">
-   </div>
-   <h1 class="text-lg font-bold text-gray-800 truncate">${
-     listing.title || "Untitled Auction"
-   }</h1>
-   <p class="text-sm text-gray-700 line-clamp-3">${
-     listing.description || "No description available."
-   }</p>
-   <p class="text-sm text-gray-700 line-clamp-3">${
-     currentBid || "No bids available."
-   }</p>
-   <div class="flex gap-4">
-     <button class="edit-btn bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-600">Edit Details</button>
-  <button class="details-btn bg-yellow-500 text-white px-3 py-1 rounded hover:bg-yellow-600">Details</button>
-  <button class="delete-btn bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600">Delete</button>
-    </div>
-    <hr class="my-4 border-gray-300" />
+  <div class ="w-1/3 max-w-60">
+    <img src="${
+      media[0] ? media[0].url : "https://via.placeholder.com/400x300"
+    }"
+    alt="${media[0] ? media[0].alt : "Auction image"}"
+    class="w-full h-full object-cover rounded-lg shadow-lg">
+  </div>
+  <div class=" flex flex-col justify-between ">
+  <div class="flex flex-col gap-2 text-sm">
+    <h1 class=""font-regular text-gray-800 truncate">${
+      listing.title || "Untitled Auction"
+    }</h1>
+    <p class="text-sm text-gray-700 line-clamp-3">${
+      listing.description || "No description available."
+    }</p>
+    <p class="text-sm text-gray-700 line-clamp-3">${
+      currentBid || "No bids available."
+    }</p>
+  </div>
+  <div class="flex gap-2 ">
+  <button class="edit-btn bg-blue-500 text-white px-2 py-1 sm:px-3 sm:text-sm rounded hover:bg-blue-600 ">Edit </button>
+  <button class="details-btn bg-yellow-500 text-white px-2 py-2 sm:px-3 sm:text-sm rounded hover:bg-yellow-600">Details</button>
+  <button class="delete-btn bg-red-500 text-white  px-2 py-1 sm:px- 3 sm:text-sm rounded hover:bg-red-600">Delete</button>
+  </div >
+  </div >
     `;
 
   console.log(listing.id, listing);
