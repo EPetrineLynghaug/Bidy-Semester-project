b3512c46e2912604aabd46b0f42173bca32c01d4
EPetrineLynghaug <136695793+EPetrineLynghaug@users.noreply.github.com>
2024-12-06 09:32:51 +0100

    fix: ensure modal pagination starts at the correct position
    Resolved an issue where the modal did not start at the top when navigating between pages. The functionality works as expected now.

--- src/js/api/auctionListings.api.js
+++ src/js/api/auctionListings.api.js
@@ -24,21 +24,6 @@ export async function fetchAuctionListings(limit = 18, page = 1) {
     console.error("Error fetching auction listings:", error.message);
   }
 }
-// export async function fetchAuctionListings(limit = 18, page = 1) {
-//   const url = `${API_AUCTION_LISTINGS}?sort=created&sortOrder=desc&limit=${limit}&page=${page}&_seller=true&_bids=true&_active=true`;
-//   try {
-//     const response = await fetch(url, {
-//       method: "GET",
-//       headers: createHeaders(),
-//     });
-//     if (!response.ok)
-//       throw new Error(`Failed to fetch listings: ${response.status}`);
-//     const result = await response.json();
-//     return result.data;
-//   } catch (error) {
-//     console.error("Error fetching auction listings:", error.message);
-//   }
-// }
 
 export async function readeProfiles(name, page = 1) {
   try {
